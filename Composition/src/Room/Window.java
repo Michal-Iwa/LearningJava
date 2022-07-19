6808423e97c471c604de2d8242c5cd2c4081cbef
Michał Iwaniak <michalpatrykiwaniak@gmail.com>
2022-07-19 17:03:24 +0200

    day 5 Composition

---
+++ Composition/src/Room/Window.java
@@ -0,0 +1,30 @@
+package Room;
+
+public class Window {
+    private String materialWindowFrame;
+    private String materialWindow;
+    private int rateView;
+
+    public Window(String materialWindowFrame, String materialWindow, int rateView) {
+        this.materialWindowFrame = materialWindowFrame;
+        this.materialWindow = materialWindow;
+        this.rateView = rateView;
+    }
+    public void openWindow(){
+        System.out.println("Window opened");
+    }
+    public void closeWindow(){
+        System.out.println("Window closed");
+    }
+    public String getMaterialWindowFrame() {
+        return materialWindowFrame;
+    }
+
+    public String getMaterialWindow() {
+        return materialWindow;
+    }
+
+    public int getRateView() {
+        return rateView;
+    }
+}
