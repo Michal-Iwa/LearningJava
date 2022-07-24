cb254bb27b5943129bf8099bb9a48b80c1deab27
Michał Iwaniak <michalpatrykiwaniak@gmail.com>
2022-07-24 21:23:27 +0200

    day 10 using Collections framework

--- JavaCollections/src/com/Collections/Main.java
+++ JavaCollections/src/com/Collections/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olympian",8,10);
         theatre.getSeats();
+        
         if(theatre.reserveSeat("A02")){
             System.out.println("Pay for the seat");
         }
